547f5fccf491c430aff89d2c795f9cd1f7d77e0a
xinxisimple <xinxisimple@163.com>
2018-07-23 18:16:42 +0800

    增加常量包

---
+++ livemq-api/src/main/java/org/livemq/api/constant/Charsets.java
@@ -0,0 +1,10 @@
+package org.livemq.api.constant;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public interface Charsets {
+
+	Charset UTF_8 = StandardCharsets.UTF_8;
+	
+}
--- livemq-api/src/main/java/org/livemq/api/spi/common/CacheManager.java
+++ livemq-api/src/main/java/org/livemq/api/spi/common/CacheManager.java
@@ -1,14 +1,44 @@
 package org.livemq.api.spi.common;
 
+import java.util.List;
+
+/**
+ * 
+ * @Title CacheManager
+ * @Package org.livemq.api.spi.common
+ * @Description TODO
+ * @author xinxisimple@163.com
+ * @date 2018-07-23 17:52
+ * @version 1.0.0
+ */
 public interface CacheManager {
 
 	void init();
 	
 	void destroy();
 	
-	void set(String key, String value);
-	
-	void set(String key, String value, int expireTime);
+	String set(String key, String value);
 	
 	String get(String key);
+	
+	Boolean exists(String key);
+	
+	Long expire(String key, int seconds);
+	
+	Long ttl(String key);
+
+	Long incr(String key);
+
+	Long hset(String key, String field, String value);
+	
+	String hget(String key, String field);
+	
+	Long hdel(String key, String ... field);
+	
+	Boolean hexists(String key, String field);
+	
+	List<String> hvals(String key);
+	
+	Long del(String key);
+	
 }
